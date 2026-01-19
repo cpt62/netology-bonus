f3ca84ad94e20c7afd0be5c35d53b649527c4d08
Alex Obmachevsky <Alexey Obmamachevsky>
2026-01-19 23:23:12 +0300

    binarySearch added from my account replit.com

---
+++ src/bonus/algo/binarySearch/Main.java
@@ -0,0 +1,30 @@
+package bonus.algo.binarySearch;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] prices = {13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
+
+        System.out.println("Для 31: " + countMore(prices, 31)); // 0
+        System.out.println("Для 26: " + countMore(prices, 26)); // 2
+        System.out.println("Для 25: " + countMore(prices, 25)); // 2
+        System.out.println("Для 20: " + countMore(prices, 20)); // 8
+    }
+
+    public static int countMore(int[] prices, int money) {
+        if (money < prices[0]) return prices.length;
+
+        if (money > prices[prices.length - 1]) return 0;
+
+        int left = 0;
+        int right = prices.length - 1;
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (prices[middle] <= money) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+        return prices.length - left;
+    }
+}
