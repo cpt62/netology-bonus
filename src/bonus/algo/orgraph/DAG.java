8c10d11f07d103afecd3ff1fd28867d829d5f202
cptJackSparrow <rgrturzn7316@gmail.com>
2026-01-31 16:34:56 +0300

    orgraph homework ended

--- src/bonus/algo/orgraph/DAG.java
+++ src/bonus/algo/orgraph/DAG.java
@@ -24,16 +24,28 @@ public class DAG<T> {
         Set<Vertex<T>> added = new HashSet<>(); // запоминаем все когда-либо побывавшие в очереди обхода вершины чтобы не дублироваться
         queue.add(from); // начинать будем из города отправления
 
+
         while (!queue.isEmpty()) { // пока очередь не пуста
-            Vertex<T> v = ... // вынимаем следующий элемент из очереди
-
+            Vertex<T> v = queue.peek(); // вынимаем следующий элемент из очереди
             // если v это город назначения, то возвращаем из paths готовый ответ для этой вершины
-
+            if (v.equals(to)) return paths.get(v);
             // перебираем вершины по дугам из v
+            for (Vertex<T> vertex : v.getAdjacent()) {
+                if (vertex.equals(to)) {
+                    paths.put(v, paths.get(v) + 1);
+                    return paths.get(v);
+                }
+                if (!paths.containsKey(vertex)) {
+                    paths.put(vertex,1);
+                }
+
+                if (!added.contains(vertex)) queue.add(vertex);
+            }
+            queue.remove(v);
+            added.add(v);
             // если такую вершину уже добавляли в очередь (воспользуйтесь коллекцией added), то пропускаем её.
             // иначе, добавляем её в очередь, added и paths (подумайте, сколько перелётов будет до этого города, если мы знаем сколько перелётов до v)
         }
-
         return -1;
     }
 
