ec5487e2d548a76b4f7e39a2cf659be0952d6357
cptJackSparrow <rgrturzn7316@gmail.com>
2026-02-12 21:53:35 +0300

    task 2 ended

---
+++ src/ru/netology/homeworks/basic/CustomService.java
@@ -0,0 +1,19 @@
+package ru.netology.homeworks.basic;
+
+public class CustomService {
+
+    //Вынес ставку пошлины в отдельную статическую переменную
+    private static int DUTY;
+
+    public static int calcDuty(int price, int weight) {
+        return price / 100 + weight * 100;
+    }
+
+    public static void setDUTY(int DUTY) {
+        CustomService.DUTY = DUTY;
+    }
+
+    public static int getDUTY() {
+        return DUTY;
+    }
+}
---
+++ src/ru/netology/homeworks/basic/task2/Main.java
@@ -0,0 +1,54 @@
+package ru.netology.homeworks.basic.task2;
+
+import ru.netology.homeworks.basic.CustomService;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Приветствую Вас, уважаемый преподаватель! Давайте подсчитаем стоимость пошлины?");
+        System.out.print("Введите \"да\" или \"нет\" : ");
+        while (true) {
+            switch (sc.nextLine().toLowerCase()) {
+                case ("нет"):
+                    System.out.println("Хорошо. Возвращайтесь в другой раз!");
+                    return;
+                case ("да"):
+                    int price, weight;
+                    System.out.print("Введите стоимость товара в рублях: ");
+                    while (true) {
+                        if (sc.hasNextInt()) {
+                            price = sc.nextInt();
+                            System.out.print("Отлично! Теперь введите вес товара в килограммах: ");
+                            while (true) {
+                                if (sc.hasNextInt()) {
+                                    weight = sc.nextInt();
+                                    System.out.println("Начинаем подсчет пошлины...");
+                                    break;
+                                } else {
+                                    System.out.print("Вы ввели не корректное значение, попробуйте ещё раз: ");
+                                    sc.next();
+                                }
+                            }
+                        } else {
+                            System.out.print("Вы ввели не корректное значение, попробуйте ещё раз: ");
+                            sc.next();
+                            continue;
+                        }
+                        break;
+                    }
+                    CustomService.setDUTY(CustomService.calcDuty(price, weight));
+                    System.out.printf("Сумма пошлина составляет %d рублей", CustomService.getDUTY());
+                    break;
+                default:
+                    System.out.print("Вы ввели неверное значение, попробуйте снова: ");
+                    continue;
+            }
+            break;
+        }
+    }
+}
+
