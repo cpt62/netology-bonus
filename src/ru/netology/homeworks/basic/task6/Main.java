cf5b7afe99ea401ba1844de3033be26d08cc68c0
cptJackSparrow <rgrturzn7316@gmail.com>
2026-03-24 22:31:51 +0300

    Добавил метод транспонирования матрицы на 180 градусов

--- src/ru/netology/homeworks/basic/task6/Main.java
+++ src/ru/netology/homeworks/basic/task6/Main.java
@@ -7,7 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
         Matrix matrix = new Matrix();
-        matrix.getMatrix();
+        int[][] resultMatrix;
+
+        System.out.println("Транспонируем матрицу на 90° по часовой");
+        resultMatrix = matrix.turn90ClockWise();
+        matrix.showMatrix(resultMatrix);
+
+        System.out.println("Транспонируем матрицу на 180° по часовой");
+        resultMatrix = matrix.turn180ClockWise();
+        matrix.showMatrix(resultMatrix);
+
+        System.out.println("Транспонируем матрица на 270° по часовой");
+        resultMatrix = matrix.turn270ClockWise();
+        matrix.showMatrix(resultMatrix);
     }
 
 }
--- src/ru/netology/homeworks/basic/task6/Matrix.java
+++ src/ru/netology/homeworks/basic/task6/Matrix.java
@@ -1,16 +1,20 @@
 package ru.netology.homeworks.basic.task6;
 
-import java.util.Arrays;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Random;
 
 public class Matrix {
 
-    private static final int SIZE = 8;
+    public static final int SIZE = 8;
 
     private final int[][] COLORS = new int[SIZE][SIZE];
 
     public Matrix() {
         matrixInit();
+        System.out.println("        Исходная матрица");
+        showMatrix(this.COLORS);
+
     }
 
     private void matrixInit() {
@@ -22,18 +26,42 @@ public class Matrix {
         }
     }
 
-    public void getMatrix() {
-        System.out.println();
-        System.out.println("-----Исходная матрица:------");
-        for (int[] ints : COLORS) {
+
+    public int[][] getCOLORS() {
+        return COLORS;
+    }
+
+    public void showMatrix(int[][] matrix) {
+        System.out.println("---------------------------------");
+        for (int[] ints : matrix) {
             for (int i : ints) {
-                System.out.print(i + " ");
+                System.out.format("%4d", i);
             }
             System.out.println();
         }
-        System.out.println("----------------------------");
+        System.out.println();
     }
 
+    public int[][] turn90ClockWise() {
+        int[][] turnedMatrix = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int k = SIZE - 1, j = 0; k >= 0; k--, j++) {
+                turnedMatrix[i][j] = COLORS[k][i];
+            }
+        }
+
+        return turnedMatrix;
+    }
+
+    public int[][] turn180ClockWise() {
+        int[][] turnedMatrix = new int[SIZE][SIZE];
+        for (int i = SIZE - 1, m = 0; i >= 0; i--, m++) {
+            for (int j = SIZE - 1, k = 0; j >= 0; j--, k++) {
+                turnedMatrix[m][k] = COLORS[i][j];
+            }
+        }
+        return turnedMatrix;
+    }
 
 
 }
