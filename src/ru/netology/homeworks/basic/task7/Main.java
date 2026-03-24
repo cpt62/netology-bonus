52ac618902ed787b18fb59185514c3febd644863
cptJackSparrow <rgrturzn7316@gmail.com>
2026-03-24 23:26:13 +0300

    Добавил интерактив выбора действий с матрицей, согласно задания

--- src/ru/netology/homeworks/basic/task7/Main.java
+++ src/ru/netology/homeworks/basic/task7/Main.java
@@ -1,21 +1,50 @@
 package ru.netology.homeworks.basic.task7;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Matrix matrix = new Matrix();
         int[][] resultMatrix;
 
-        System.out.println("Транспонируем матрицу на 90° по часовой");
-        resultMatrix = matrix.turn90ClockWise();
-        matrix.showMatrix(resultMatrix);
+        System.out.println("Выберите действие с матрицей:");
+        System.out.println("1 - транспонирование исходной матрицы на 90° по часовой стрелке");
+        System.out.println("2 - транспонирование исходной матрицы на 180° по часовой стрелке");
+        System.out.println("3 - транспонирование исходной матрицы на 270° по часовой стрелке");
+        System.out.println("end - завершить работу программы");
+        System.out.println();
 
-        System.out.println("Транспонируем матрицу на 180° по часовой");
-        resultMatrix = matrix.turn180ClockWise();
-        matrix.showMatrix(resultMatrix);
+        Scanner s = new Scanner(System.in);
 
-        System.out.println("Транспонируем матрицу на 270° по часовой");
-        resultMatrix = matrix.turn270ClockWise();
-        matrix.showMatrix(resultMatrix);
+        while (true) {
+            String input = s.nextLine().toLowerCase();
+            if (input.equals("end")) return;
+            try {
+                switch (Integer.parseInt(input)) {
+                    case 1 -> {
+                        System.out.println("Транспонируем исходную матрицу на 90° по часовой");
+                        resultMatrix = matrix.turn90ClockWise();
+                        matrix.showMatrix(resultMatrix);
+                    }
+                    case 2 -> {
+                        System.out.println("Транспонируем исходную матрицу на 180° по часовой стрелке");
+                        resultMatrix = matrix.turn180ClockWise();
+                        matrix.showMatrix(resultMatrix);
+                    }
+                    case 3 -> {
+                        System.out.println("Транспонируем исходную матрицу на 270° по часовой");
+                        resultMatrix = matrix.turn270ClockWise();
+                        matrix.showMatrix(resultMatrix);
+                    }
+                    default -> {
+                        System.out.println("Введите корректное значение действия!");
+                        System.out.println();
+                    }
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Введите корректное значение действия!");
+            }
+        }
     }
 }
